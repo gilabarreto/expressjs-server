a00c8983eb921d3365ca20a7d5e31f5e03998dc0
Victor Gila Barreto <gilabarreto@gmail.com>
2022-12-28 16:31:24 -0700

    Create hard coded database

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const express = require('express');
+const res = require('express/lib/response');
 const path = require('path');
 
 const app = express();
+
+const members = [
+  {
+    id: 1,
+    name: 'Victor GB',
+    email: 'vgb@vgb.com',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Tyler Durden',
+    email: 'td@td.com',
+    status: 'inactive'
+  },
+  {
+    id: 3,
+    name: 'Marla Singer',
+    email: 'ms@ms.com',
+    status: 'active'
+  }
+]
+
+app.get('/api/members', (req, res) => {
+  res.json(members);
+});
 
 // app.get('/', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'public', 'index.html'))
