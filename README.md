1714e9456c3b391f1d32adeca4737a7320129d9d
Victor Gila Barreto <gilabarreto@gmail.com>
2022-12-28 19:22:25 -0700

    Implement CRUD model

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # expressjs-server
+
+Simple Express.js server and some example files for future reference.
+
+## About Express.js
+
+"Express.js, or simply Express, is a back end web application framework for building RESTful APIs with Node.js, released as free and open-source software under the MIT License. It is designed for building web applications and APIs. It has been called the de facto standard server framework for Node.js." (Wikipedia)
--- routes/api/members.js
+++ routes/api/members.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const members = require('../../Members');
-const router = express.Router();
 const uuid = require('uuid');
+const router = express.Router();
+const members = require('../../Members');
 
 // Get All Members
 router.get('/', (req, res) => res.json(members));
@@ -26,11 +26,43 @@ router.post('/', (req, res) => {
   }
 
   if (!newMember.name || !newMember.email) {
-    res.status(400).json({ msg: 'Please include a name and email' })
+    return res.status(400).json({ msg: 'Please include a name and email' })
   }
   members.push(newMember);
   res.json(members);
 
+  // Update Member
+  router.put('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if (found) {
+      const updMember = req.body;
+      members.forEach(member => {
+        if (member.id === parseInt(req.params.id)) {
+          member.name = updMember.name ? updMember.name : member.name;
+          member.email = updMember.email ? updMember.email : member.email;
+
+          res.json({ msg: 'Member updated', member })
+        }
+      });
+    } else {
+      res.status(400).json({ msg: `No member with the id of ${req.params.id}` })
+    }
+  });
+
+});
+
+// Delete Member
+router.delete('/:id', (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json({
+      msg: 'Member deleted',
+      members: members.filter(member => member.id !== parseInt(req.params.id))
+    });
+  }
+  return res.status(400).json({ msg: `No member with the id of ${req.params.id}` })
 });
 
 module.exports = router;
